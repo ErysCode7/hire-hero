5fb1536761a1b388a57f1b7569fe0bb550ce2309
Erys Mozo <mozoerys@gmail.com>
2024-05-09 08:19:09 +0800

    added tanstack query and query provider

---
+++ src/providers/query-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type QueryProdiverProps = {
+  children: ReactNode;
+};
+
+export const QueryProdiver = ({ children }: QueryProdiverProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
