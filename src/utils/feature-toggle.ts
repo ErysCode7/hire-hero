5a56a79605594676ac543415257ea93388a0bf2a
Erys Mozo <mozoerys@gmail.com>
2024-05-09 08:20:32 +0800

    added utils folder

---
+++ src/utils/feature-toggle.ts
@@ -0,0 +1,14 @@
+const DEFAULT_FEATURE_TOGGLE = {
+  TAILWIND_INDICATOR: false,
+};
+
+const getFeatureToggle = () => {
+  try {
+    if (process.env.NEXT_PUBLIC_FEATURE_TOGGLE) {
+      return JSON.parse(process.env.NEXT_PUBLIC_FEATURE_TOGGLE);
+    }
+  } catch (error) {}
+  return DEFAULT_FEATURE_TOGGLE;
+};
+
+export const featureToggle = getFeatureToggle();
