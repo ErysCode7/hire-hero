b4f575b2dc83ce35cceea67da748dcd11f4366e3
Erys Mozo <mozoerys@gmail.com>
2024-05-09 08:23:04 +0800

    added constant folder

---
+++ src/constant/local-storage.ts
@@ -0,0 +1,1 @@
+export const USER_STORE_LOCAL_STORAGE_NANE = "user-store";
---
+++ src/constant/role.ts
@@ -0,0 +1,13 @@
+import { capitalizeFirstLetter } from "@/utils/helper";
+
+const USER_ROLE = {
+  SEEKER: "SEEKER",
+  SPECIALIST: "SPECIALIST",
+} as const;
+
+export type UserRole = keyof typeof USER_ROLE;
+
+const seekerRole = capitalizeFirstLetter(USER_ROLE.SEEKER);
+const specialistRole = capitalizeFirstLetter(USER_ROLE.SPECIALIST);
+
+export { USER_ROLE, seekerRole, specialistRole };
